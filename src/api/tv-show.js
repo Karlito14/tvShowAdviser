10040e4d6b03e5e4190bda8de04cfcfd9d6bbbb5
Karlito14 <leiroz26@hotmail.com>
2023-07-05 14:52:25 +0200

    suppresion de gh-pages

--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -3,17 +3,17 @@ import { API_KEY, BASE_URL } from "../config";
 
 export class TVShowAPI {
    static async fetchPopulars() {
-      const response = await axios.get(`${BASE_URL}/tv/popular/${API_KEY}`); 
+      const response = await axios.get(`${BASE_URL}/tv/popular/?api_key=${API_KEY}`); 
       return response.data.results;
    }
 
    static async fetchRecommendations(tvShowId) {
-      const response = await axios.get(`${BASE_URL}/tv/${tvShowId}/recommendations${API_KEY}`); 
+      const response = await axios.get(`${BASE_URL}/tv/${tvShowId}/recommendations?api_key=${API_KEY}`); 
       return response.data.results;
    }
 
    static async fetchByTitle(title) {
-      const response = await axios.get(`${BASE_URL}/search/tv${API_KEY}&query=${title}`); 
+      const response = await axios.get(`${BASE_URL}/search/tv?api_key=${API_KEY}&query=${title}`); 
       return response.data.results;
    }
 }
